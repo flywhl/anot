d9c0b55f6d4115d5584a2cc91e81c78a7d884daf
synapticsurge <synapticsurge@proton.me>
2025-01-22 20:30:18 +0000

    fmt

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,13 +1,21 @@
+use anot;
 use anyhow::Ok;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use anot;
 
 fn benchmark_cli_execution(c: &mut Criterion) {
     c.bench_function("Benchmark results", |b| {
         b.iter(|| {
-                let args: Vec<String> = black_box(["./target/release/anot".to_string(), "./rust".to_string(), "--tags".to_string(), "hypothesis,note,todo".to_string()].to_vec()); 
-                anot::cli::run(args)?;
-                Ok(())
+            let args: Vec<String> = black_box(
+                [
+                    "./target/release/anot".to_string(),
+                    "./rust".to_string(),
+                    "--tags".to_string(),
+                    "hypothesis,note,todo".to_string(),
+                ]
+                .to_vec(),
+            );
+            anot::cli::run(args)?;
+            Ok(())
         })
     });
 }
