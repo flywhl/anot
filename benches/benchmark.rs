8f0c5ec7a40c396751f6707f88d6036cbf771f52
synapticsurge <synapticsurge@proton.me>
2025-01-22 20:53:12 +0000

    fixed timeout

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,13 +2,13 @@ use anot;
 use anyhow::Ok;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn benchmark_cli_execution(c: &mut Criterion) {
+fn benchmark_execution(c: &mut Criterion) {
     c.bench_function("Benchmark results", |b| {
         b.iter(|| {
             let args: Vec<String> = black_box(
                 [
                     "./target/release/anot".to_string(),
-                    "./rust".to_string(),
+                    "./alacritty/".to_string(),
                     "--tags".to_string(),
                     "hypothesis,note,todo".to_string(),
                 ]
@@ -20,5 +20,5 @@ fn benchmark_cli_execution(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, benchmark_cli_execution);
+criterion_group!(benches, benchmark_execution);
 criterion_main!(benches);
