d12b21cabca86f4f42abc3b048379121b5339b2d
synapticsurge <synapticsurge@proton.me>
2025-01-22 20:28:49 +0000

    fixed

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,17 +1,13 @@
+use anyhow::Ok;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::process::Command;
+use anot;
 
 fn benchmark_cli_execution(c: &mut Criterion) {
     c.bench_function("Benchmark results", |b| {
         b.iter(|| {
-            let output = Command::new("./target/release/anot")
-                .arg("./rust/")
-                .arg("--tags")
-                .arg("hypothesis,note,todo")
-                .output()
-                .expect("Failed to execute");
-            //println!("{:?}", output);
-            black_box(output);
+                let args: Vec<String> = black_box(["./target/release/anot".to_string(), "./rust".to_string(), "--tags".to_string(), "hypothesis,note,todo".to_string()].to_vec()); 
+                anot::cli::run(args)?;
+                Ok(())
         })
     });
 }
